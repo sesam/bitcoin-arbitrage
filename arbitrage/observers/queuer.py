f921958ccc9ed5d3d1556a134f006d9899e355c6
Luís Martinho <lmartinho@gmail.com>
2014-02-22 21:14:56 -0800

    defaulting to localhost

--- arbitrage/observers/queuer.py
+++ arbitrage/observers/queuer.py
@@ -5,7 +5,7 @@ import pymongo
 from .observer import Observer
 
 from pymongo import MongoClient
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient()
 db = client.ng_bitcoin_arbitrage
 
 class Queuer(Observer):
