2b93867f1078bb1a45943d626b68faec11de30f4
Luís Martinho <lmartinho@gmail.com>
2014-02-22 21:12:32 -0800

    custom observer used to feed a queue (using a mongo collection for now)

---
+++ arbitrage/observers/queuer.py
@@ -0,0 +1,34 @@
+import json
+import logging
+import datetime
+import pymongo
+from .observer import Observer
+
+from pymongo import MongoClient
+client = MongoClient('mongodb://localhost:27017/')
+db = client.ng_bitcoin_arbitrage
+
+class Queuer(Observer):
+    db = None
+
+    def begin_opportunity_finder(self, depths):
+        logging.info("begin finder")
+
+    def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc,
+                    weighted_buyprice, weighted_sellprice):
+        logging.info("profit: %f USD with volume: %f BTC - buy at %.4f (%s) sell at %.4f (%s) ~%.2f%%" % (profit, volume, buyprice, kask, sellprice, kbid, perc))
+        
+        # TODO: invalidate an opportunity after the price changes
+        db.opportunities.update({"kbid" : kbid, "kask" : kask}, {
+            "profit" : profit,
+            "volume" : volume,
+            "buyprice" : buyprice,
+            "kask" : kask,
+            "sellprice" : sellprice,
+            "kbid" : kbid,
+            "perc" : perc,
+            "weighted_buyprice" : weighted_buyprice,
+            "weighted_sellprice" : weighted_sellprice,
+            "updated_on" : datetime.datetime.utcnow()
+        }, upsert=True)
+
